2db85d4c27ce6d87660d36f02793810994b075d5
kokbanan <kokbanan@fake.com>
2015-06-06 11:49:33 +0600

    1

--- python/test_example.py
+++ python/test_example.py
@@ -4,18 +4,18 @@ import unittest
 
 class ExampleTest(BaseTestCase):
     def test_example(self):
-        main_window = self.driver.find_element_by_id('MainWindow')
+        main_window = self.driver.find_element_by_class_name('Window')
 
-        search_string = main_window.find_element_by_id('QueryMW')
-        search_string.send_keys('3')
+        search_string = main_window.find_element_by_class_name('TextBox')
+        search_string.send_keys('Son')
 
-        search_button = main_window.find_element_by_id('SearchMW')
+        search_button = main_window.find_element_by_class_name('Button')
         search_button.click()
 
-        products_list = main_window.find_element_by_id('ProductsMW')
+        products_list = main_window.find_element_by_id('ListView')
         product_items = products_list.find_elements_by_class_name('ListViewItem')
 
-        self.assertEqual(len(product_items), 1)
+        self.assertEqual(len(product_items), 3)
 
 
 if __name__ == '__main__':
